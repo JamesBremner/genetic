e1ac77d37f6d1cf1ef4e4fcdc2832f527c890145
James Bremner <james@ravenspoint.com>
2018-09-12 18:37:58 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <bitset>         // std::bitset
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/// specify population size
+const int population_size = 50;
+
+/// chromsone
+typedef bitset<20> chromo_t;
+
+/// a poulation of chromosones
+vector< chromo_t > population;
+
+/// generate random chromosone
+chromo_t generate()
+{
+    chromo_t c;
+    for( int k = 0; k<20; k++ )
+        c.set( k, rand()%2 );
+    return c;
+}
+
+/// generate random initial population
+void generate_initial_population()
+{
+    for( int k = 0; k < population_size; k++ )
+    {
+        population.push_back( generate() );
+    }
+}
+
+/// calculate fitnesss by counting set bits in chromo
+int fitness( chromo_t c  )
+{
+    int count = 0;
+    for( int k = 0; k<20; k++ )
+        if( c[ k ] )
+            count++;
+    return count;
+}
+
+/// inverse fitness
+int fitness_inv( chromo_t c  )
+{
+    int count = 0;
+    for( int k = 0; k<20; k++ )
+        if( ! c[ k ] )
+            count++;
+    return count;
+}
+
+/// Fitness proportionate selection, also known as roulette wheel selection
+/// https://en.wikipedia.org/wiki/Fitness_proportionate_selection
+vector< double > selection_probability()
+{
+    vector< double > probs;
+    double total = 0;
+    for( auto& c : population )
+    {
+        total += fitness( c );
+    }
+    for( auto& c : population )
+    {
+        probs.push_back( fitness( c ) / total );
+    }
+//    for( auto p : probs )
+//    {
+//        cout << p <<" ";
+//    }
+//    cout << "\n";
+    return probs;
+}
+
+/// inverse selection probability
+vector< double > selection_probability_inv()
+{
+    vector< double > probs;
+    double total = 0;
+    for( auto& c : population )
+    {
+        total += fitness_inv( c );
+    }
+    for( auto& c : population )
+    {
+        probs.push_back( fitness_inv( c ) / total );
+    }
+//    for( auto p : probs )
+//    {
+//        cout << p <<" ";
+//    }
+//    cout << "\n";
+    return probs;
+}
+
+/// select from population using roulette wheel probabilities
+int select(  const vector< double >& probs )
+{
+    int r = rand() % 99;
+    int limit = probs[0]*100;
+    for( int k = 0; k < probs.size(); k++ )
+    {
+        //cout << r <<" "<< limit << "\n";
+        if( r < limit )
+        {
+            //cout << k << "\n";
+            return k;
+        }
+        limit += probs[k+1] * 100;
+    }
+    return probs.size() - 1;
+}
+
+/// test roulette wheel selection
+void select_as_parent_test()
+{
+    vector< double > probs { 0.1, 0.2, 0.3, 0.4 };
+    vector< int > hist( 4 );
+    for( int k = 0; k<10000; k++ )
+    {
+        hist[ select( probs ) ]++;
+    }
+    bool fOK = true;
+    for( int k = 0; k < 4; k++ )
+    {
+        if( fabs( hist[k] - 10000 * probs[k] ) > 150 )
+            fOK = false;
+    }
+    if( ! fOK )
+    {
+        for( int h : hist )
+            cout << h << " ";
+        cout << "\n";
+        throw runtime_error("select_as_parent failed unit test");
+    }
+}
+
+
+// mutate chromosome
+chromo_t mutate( chromo_t& c )
+{
+    chromo_t child = c;
+    for( int k = 0; k < 20; k++ )
+    {
+        if( rand()%2 )
+            child[k] = ! child[k];
+    }
+    return child;
+}
+
+// simulate breeding
+void breed( )
+{
+    // calculate selection probabilities
+    vector< double > probs = selection_probability();
+    vector< double > probs_inv = selection_probability_inv();
+
+    // half the population breeds
+    for( int k = 0; k < population_size / 2; k++ )
+    {
+        // parent selection based on fitness
+        int parent = select( probs );
+
+        // remove bases on inverse fitness
+        int dead = select( probs_inv );
+
+        // mutate and replace
+        population[ dead ] = mutate( population[ parent ] );
+    }
+}
+
+// Display population
+void Display()
+{
+    int ftotal = 0;
+    int maxf   = 0;
+    for( int k = 0; k < population_size; k++ )
+    {
+        int f = fitness( population[k] );
+        ftotal += f;
+        if( f > maxf )
+            maxf = f;
+//        cout << population[k]
+//             <<" fitness " << f
+//             << "\n";
+    }
+    cout << "average fitness " << (double)ftotal/ population_size << " max " << maxf << "\n";
+
+    // stop when perfection reached
+    if( maxf == 20 )
+    {
+        for( int k = 0; k < population_size; k++ )
+        {
+            int f = fitness( population[k] );
+            cout << population[k]
+                 <<" fitness " << f
+                 << "\n";
+        }
+        exit( 0 );
+    }
+}
+
+
+int main()
+{
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    // test the roulette wheel
+    select_as_parent_test();
+
+    // initial population
+    generate_initial_population();
+
+    // for generations
+    for( int gen = 0; gen < 10000; gen++ )
+    {
+        Display();
+
+        breed(  );
+    }
+
+    return 0;
+}
